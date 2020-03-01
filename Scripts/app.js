2ea49cf893421beaea35cebc30fe0980c1978981
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-03-01 01:38:12 -0500

    Added links and few examples of jquery

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,7 +15,28 @@ let app;
      */
     function Start()
     {
-        
+        let mainContent = $("#mainContent")[0];
+        console.log(mainContent);
+
+        let mainCont = document.getElementById("mainContent");
+        console.log(mainCont);
+
+        let mainQuery = document.querySelectorAll("#mainContent")[0];
+        console.log(mainQuery);
+
+        let mainFirstQuery = document.querySelector("#mainContent");
+        console.log(mainFirstQuery);
+
+        //$(".display-4").hide();   //it hide the display-4 "Hello World"
+
+        //console.log($(".nav-link").css("font-weight","bold")); //it make bold all the items of nave links
+
+        //let navlink = document.getElementsByClassName("nav-link"); To hide the first element from nav-link
+
+        //navlink[0].style.display = "none";
+
+        //$(".nav-link").hide();
+
         Main();
     }
 
